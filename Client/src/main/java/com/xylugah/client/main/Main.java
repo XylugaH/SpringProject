117d48461d7ac0fbf3a056d6feb91034931b0d6f
Андрей <@192.168.1.2>
2016-10-30 22:33:51 +0300

    Refactoring

--- Client/src/main/java/com/xylugah/client/main/Main.java
+++ Client/src/main/java/com/xylugah/client/main/Main.java
@@ -8,15 +8,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.xylugah.client.core.Controller;
-import com.xylugah.springcore.transport.Transport;
 
 public class Main {
 	public static final ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
 	public static final int port = 1238;
 
 	public static void main(String[] args) throws IOException {
-		Transport myClass = (Transport) Main.context.getBean("Transport");
-		myClass.receive();
 		try (ServerSocket socketListener = new ServerSocket(port)) {
 			while (true) {
 				System.out.println("Waiting connection ...");
