5366f4a5b66a6e24cbd2d75acf918bf45e023633
Андрей <@192.168.1.2>
2016-10-30 17:19:51 +0300

    Add serialVersionUID

--- Core/src/main/java/com/xylugah/springcore/core/GetEnvironmentsRequest.java
+++ Core/src/main/java/com/xylugah/springcore/core/GetEnvironmentsRequest.java
@@ -1,6 +1,8 @@
 package com.xylugah.springcore.core;
 
 public class GetEnvironmentsRequest extends Request {
+
+	private static final long serialVersionUID = -931614060572700581L;
 	private final Action action = Action.GET_INVIRONMENTS;
 
 	@Override
--- Core/src/main/java/com/xylugah/springcore/core/GetStatisticsRequest.java
+++ Core/src/main/java/com/xylugah/springcore/core/GetStatisticsRequest.java
@@ -1,6 +1,7 @@
 package com.xylugah.springcore.core;
 
 public class GetStatisticsRequest extends Request {
+	private static final long serialVersionUID = -6088213404326495758L;
 	private final Action action = Action.GET_STATISTICS;
 
 	@Override
