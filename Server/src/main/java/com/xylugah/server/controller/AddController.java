61e1c64100d6e59fe0102327b96023eff282cca2
Андрей <@192.168.1.2>
2016-11-06 22:05:35 +0300

    Add Client class

--- Server/src/main/java/com/xylugah/server/controller/AddController.java
+++ Server/src/main/java/com/xylugah/server/controller/AddController.java
@@ -13,28 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 public class AddController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public AddController() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("ADD: ").append(request.getContextPath());
+		int id = 0;
+		// try {
+		id = Integer.valueOf(request.getParameter("id"));
+		// } catch (NumberFormatException e) {
+		// id = 1;
+		// }
+		response.getWriter().append(id + "ADD: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
---
+++ Server/src/main/java/com/xylugah/server/core/Client.java
@@ -0,0 +1,32 @@
+package com.xylugah.server.core;
+
+public class Client {
+	private String ip;
+	private int port;
+
+	public Client() {
+		super();
+	}
+
+	public Client(final String ip, final int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
