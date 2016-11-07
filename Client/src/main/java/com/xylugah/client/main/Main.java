a11076c045758df2fc55de4edb92cc115b8dddb7
Андрей <@192.168.1.2>
2016-11-07 16:17:03 +0300

    Refactoring some nemase of package

--- Client/src/main/java/com/xylugah/client/main/Main.java
+++ Client/src/main/java/com/xylugah/client/main/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,14 +13,15 @@ import com.xylugah.client.core.Controller;
 public class Main {
 	public static final ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
 	public static final int port = 1238;
+	public static final Logger logger = Logger.getLogger(Main.class.getName());
 
 	public static void main(String[] args) throws IOException {
 
 		try (ServerSocket socketListener = new ServerSocket(port)) {
 			while (true) {
-				System.out.println("Waiting connection ...");
+				logger.debug("Waiting connection ...");
 				Socket socket = socketListener.accept();
-				System.out.println("Connection sucessfull !!!");
+				logger.debug("Connection sucessfull !!!");
 				new Controller(socket);
 			}
 		}
--- Server/src/main/java/com/xylugah/server/servlet/EnvironmentsController.java
+++ Server/src/main/java/com/xylugah/server/servlet/EnvironmentsController.java
@@ -7,17 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class EnvironmentsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		response.getWriter().append("Environments: ").append(request.getContextPath());
+		ServletContext servletContext = 
+		WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+				SampleService service = (SampleService) webApplicationContext.getBean("sampleService");
+		response.getWriter().append("Environments: " + data.getById(0).getIp());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
