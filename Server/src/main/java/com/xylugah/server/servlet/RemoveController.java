1fe7c190388a1ce01aed05592e47e47526b60ee3
XylugaH <XylugaH3k@gmail.com>
2016-11-09 14:23:35 +0200

    Implementation remove methods

--- Server/src/main/java/com/xylugah/server/servlet/RemoveController.java
+++ Server/src/main/java/com/xylugah/server/servlet/RemoveController.java
@@ -1,39 +1,55 @@
 package com.xylugah.server.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RemoveController
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.xylugah.springcore.dao.DataDAO;
+
 public class RemoveController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RemoveController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private DataDAO dao;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public RemoveController() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String id = request.getParameter("id");
+		PrintWriter out = response.getWriter();
+
+		if (id == null) {
+			out.println("<h2>Error: ID is empty!!!</h2>");
+		} else {
+			int idClient = Integer.valueOf(id);
+			if (dao.remove(idClient)) {
+				out.println("<h2>Remove successful!!!</h2>");
+			} else {
+				out.println("<h2>Remove error!!!</h2>");
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		ApplicationContext ac = (ApplicationContext) config.getServletContext()
+				.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		this.dao = (DataDAO) ac.getBean("DataDAO");
+	}
 }
