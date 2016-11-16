91ea1693fbb909773940b55449a8d9f793df4625
Андрей <@192.168.1.3>
2016-11-16 15:30:12 +0300

    Implements print method client to page

--- Server/src/main/java/com/xylugah/server/servlet/EnvironmentsServlet.java
+++ Server/src/main/java/com/xylugah/server/servlet/EnvironmentsServlet.java
@@ -3,6 +3,8 @@ package com.xylugah.server.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,6 +17,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.xylugah.springcore.dao.DataDAO;
+import com.xylugah.springcore.model.Client;
 import com.xylugah.springcore.model.GetStatisticsRequest;
 import com.xylugah.springcore.model.Response;
 import com.xylugah.springcore.transport.Transport;
@@ -27,37 +30,31 @@ public class EnvironmentsServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		logger.info("In EnvironmentsServlet");
-		Socket socket = new Socket("127.0.0.1", 1238);
-		transport.transmit(new GetStatisticsRequest(), socket);
-		Response res = (Response) transport.receive(socket);
+		String id = request.getParameter("id");
+		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
-		out.println(res.getFreeDiskSpace());
-		socket.close();
-		/*
-		 * String id = request.getParameter("id"); PrintWriter out =
-		 * response.getWriter(); if (id == null) { List<Client> clientList =
-		 * dao.getClientList();
-		 * 
-		 * out.println("<html><body>"); out.println("<h1>List of client</h1>");
-		 * 
-		 * if (clientList.isEmpty()) {
-		 * out.println("<h2>Client list is empty!!!</h2>"); } else { for (Client
-		 * client : clientList) { out.println("<h3> id: " + client.getId() +
-		 * "</h3>"); out.println("<h3> ip-adress: " + client.getIp() + "</h3>");
-		 * out.println("<h3> port: " + client.getPort() + "</h3>");
-		 * out.println("<table>"); out.println("</table>");
-		 * 
-		 * } } out.println("</body></html>"); } else { int intId =
-		 * Integer.valueOf(id); logger.info("Get user by id = " + intId); Client
-		 * client = dao.getById(intId); if (client == null) {
-		 * out.println("<h2>Client with ID=" + intId + " not found!!!</h2>"); }
-		 * else { out.println("<h3> ip-adress: " + client.getIp() + "</h3>");
-		 * out.println("<h3> port: " + client.getPort() + "</h3>");
-		 * out.println("<table>"); out.println("</table>"); }
-		 * 
-		 * } out.close();
-		 */
+		if (id != null) {
+			try {
+				int idClient = Integer.parseInt(id);
+				Client client = dao.getById(idClient);
+				if (client != null) {
+					printClient(client, out);
+				} else {
+					out.println("<h2>Client with id=" + idClient + " not found!</h2>");
+					logger.error("Client with id=" + idClient + " not found!");
+				}
+			} catch (Exception e) {
+				out.println("<h2>No valid id</h2>");
+				logger.error("No valid id - " + id, e);
+			}
+		} else {
+			List<Client> clientList = dao.getClientList();
+			for (Client client : clientList) {
+				printClient(client, out);
+			}
+		}
+		out.close();
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -79,5 +76,42 @@ public class EnvironmentsServlet extends HttpServlet {
 		this.dao = (DataDAO) ac.getBean("DataDAO");
 		this.transport = (Transport) ac.getBean("Transport");
 
+	}
+
+	public void printClient(final Client client, final PrintWriter out) {
+		out.println("<h3> id: " + client.getId() + "</h3>");
+		out.println("<h3> ip-adress: " + client.getIp() + "</h3>");
+		out.println("<h3> port: " + client.getPort() + "</h3>");
+		try (Socket socket = new Socket(client.getIp(), client.getPort())) {
+			transport.transmit(new GetStatisticsRequest(), socket);
+			Response res = (Response) transport.receive(socket);
+			socket.close();
+			if (res != null) {
+				out.println("<h4> Free disk space: " + res.getFreeDiskSpace() + "</h4>");
+				out.println("<h4> Free memory: " + res.getFreeMemory() + "</h4>");
+				List<String> procList = res.getProcessesList();
+				if (procList != null) {
+					out.println("<table>");
+					for (String proc : procList) {
+						StringTokenizer st = new StringTokenizer(proc, " \t\n\r,.");
+						out.println("<tr>");
+						while (st.hasMoreTokens()) {
+							out.println("<td>" + st.nextToken() + "</td>");
+						}
+						out.println("</tr>");
+					}
+					out.println("</table>");
+				} else {
+					out.println("<h2>Proccess list is empty!</h2>");
+				}
+			} else {
+				out.println("<h2>No response from client!</h2>");
+				logger.error("Response from client is null!!!");
+			}
+		} catch (IOException e) {
+			out.println("<h2>Connection failed!!!</h2>");
+			logger.error("Connection failed with client " + client.getIp() + ":" + client.getPort());
+		}
+		out.println("<hr></hr>");
 	}
 }
