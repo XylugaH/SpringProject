107f6bce34a5a8897ac63e7c1d2fd183cecf184c
Андрей <@192.168.1.2>
2016-11-19 19:03:41 +0300

    Add validate objects request, response

--- Core/src/main/java/com/xylugah/springcore/transport/SerializationTransport.java
+++ Core/src/main/java/com/xylugah/springcore/transport/SerializationTransport.java
@@ -5,9 +5,41 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import com.xylugah.springcore.model.Request;
+import com.xylugah.springcore.model.Response;
+
 public class SerializationTransport implements Transport {
 
 	@Override
+	public Request receiveRequest(Socket socket) {
+		Object obj = receive(socket);
+		if (obj instanceof Request) {
+			return (Request) obj;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void transmitRequest(Request request, Socket socket) {
+		transmit(request, socket);
+	}
+
+	@Override
+	public Response reciveResponse(Socket socket) {
+		Object obj = receive(socket);
+		if (obj instanceof Response) {
+			return (Response) obj;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void transmitResponse(Response response, Socket socket) {
+		transmit(response, socket);
+	}
+
 	public Object receive(final Socket socket) {
 		try {
 			ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
@@ -21,7 +53,6 @@ public class SerializationTransport implements Transport {
 		return null;
 	}
 
-	@Override
 	public void transmit(final Object outputObj, final Socket socket) {
 		try {
 			ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
