8e1512aaf6b92f90bf145f2ce175d30f7fbdc97a
Андрей <@192.168.1.2>
2016-11-27 20:40:41 +0300

    Add sql DAO

---
+++ Core/src/main/java/com/xylugah/springcore/dao/DataMySQLDAO.java
@@ -0,0 +1,33 @@
+package com.xylugah.springcore.dao;
+
+import java.util.List;
+
+import com.xylugah.springcore.model.Client;
+
+public class DataMySQLDAO implements DataDAO {
+
+	@Override
+	public void add(String ip, int port) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean remove(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Client getById(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> getClientList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
