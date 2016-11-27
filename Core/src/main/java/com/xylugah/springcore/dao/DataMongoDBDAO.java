59073d3422fa9721f61489ed5cdb5198566e0442
Андрей <@192.168.1.2>
2016-11-27 20:42:29 +0300

    Implement methods

--- Core/src/main/java/com/xylugah/springcore/dao/DataMongoDBDAO.java
+++ Core/src/main/java/com/xylugah/springcore/dao/DataMongoDBDAO.java
@@ -3,36 +3,90 @@ package com.xylugah.springcore.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import com.xylugah.springcore.model.Client;
 
 public class DataMongoDBDAO implements DataDAO {
+	private MongoClient mongo = null;
+	private DB db = null;
 
-	private List<Client> clientList = new ArrayList<>();
+	public DataMongoDBDAO() {
+		getConnection();
+	}
 
 	@Override
 	public void add(final String ip, final int port) {
-		System.out.println("Save to Mongo!!");
-
+		DBCollection table = db.getCollection("client");
+		BasicDBObject document = new BasicDBObject();
+		document.put("id", 2);
+		document.put("ip", ip);
+		document.put("port", port);
+		table.insert(document);
 	}
 
 	@Override
 	public boolean remove(final int id) {
-		clientList.remove(id);
-		return true;
+		DBCollection table = db.getCollection("client");
+
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("id", id);
+		DBObject cursor = table.findOne(searchQuery);
+
+		if (cursor != null) {
+			table.remove(searchQuery);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
-	public Client getById(int index) {
-		return null;
+	public Client getById(int id) {
+		DBCollection table = db.getCollection("client");
+
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("id", id);
+		DBObject cursor = table.findOne(searchQuery);
+		if (cursor != null) {
+			Client client = new Client();
+			client.setId((Integer) cursor.get("id"));
+			client.setIp(String.valueOf(cursor.get("ip")));
+			client.setPort((Integer) cursor.get("port"));
+			return client;
+		} else {
+			return null;
+		}
+
 	}
 
 	@Override
 	public List<Client> getClientList() {
-		return clientList;
+		List<Client> clientList = new ArrayList<>();
+		DBCollection table = db.getCollection("client");
+		DBCursor cursor = table.find();
+		while (cursor.hasNext()) {
+			DBObject obj = cursor.next();
+			Client client = new Client();
+			client.setId((Integer) obj.get("id"));
+			client.setIp(String.valueOf(obj.get("ip")));
+			client.setPort((Integer) obj.get("port"));
+			clientList.add(client);
+		}
+		if (clientList.isEmpty()) {
+			return null;
+		} else {
+			return clientList;
+		}
 	}
 
-	public void setClientList(List<Client> clientList) {
-		this.clientList = clientList;
+	private void getConnection() {
+		this.mongo = new MongoClient("localhost", 27017);
+		this.db = mongo.getDB("ClientDB");
 	}
 
 }
